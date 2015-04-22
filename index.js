f00d83000e5a1c2ead389e87712510ef18837679
David Cox <dcox@jive.com>
2015-04-22 07:37:51 -0600

    Must be started intentionally

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var portscanner = require('portscanner');
+var Promise = require('promise');
 // custom
 var router = require('./lib/router');
 
-var app = express();
+var Baas = function(config) {
+	if (!config) {
+		config = {};
+	}
+	var port = config.port;
+	var app = express();
 
-app.use(bodyParser.json());
+	app.use(bodyParser.json());
+	app.use('/', router);
 
-app.use('/', router);
+	var getPort = function() {
+		var promise = new Promise(function(resolve, reject) {
+			if (!port) {
+				portscanner.findAPortNotInUse(8000, 9000, '127.0.0.1', function(error, availablePort) {
+					port = availablePort;
+					resolve(port);
+				});
+			} else {
+				resolve(port);
+			}
+		});
+		return promise;
+	};
 
-portscanner.findAPortNotInUse(8000, 9000, '127.0.0.1', function(error, port) {
-	app.listen(port, function() {
-		console.log('baas listening on port ' + port);
-	});
-});
+	this.start = function() {
+		if (!port) {
+			getPort().then(function(desiredPort) {
+				app.listen(desiredPort, function() {
+					console.log('baas listening on port ' + desiredPort);
+				});
+			});
+		}
+	};
+};
+
+module.exports = Baas;
